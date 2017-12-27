0bfb111084a9e5ecf8d5cb2296ac778f834220bb
shinmarq <shin@chatbot.ph>
2017-12-27 22:56:23 +0800

    test

--- src/dialogs/events.js
+++ src/dialogs/events.js
@@ -13,7 +13,6 @@ module.exports.popularEvents = [
         builder.Prompts.choice(session, msg, card.choices(consts.cards.sample_event), consts.styles.mr_button);
     },
     (session, results) => {
-        var choices = card.choices(consts.cards.sample_event);
 
         if(!results.response) {
             session.replaceDialog('/');
@@ -43,14 +42,29 @@ module.exports.upcomingEvents = [
         var cardName = card.getName(consts.cards.sample_event);
         var msg = card(session, consts.cards.sample_event, cardName);
 
-        builder.Prompts.choice(session, msg, card.choices(consts.cards.sample_event), consts.styles.mr_button);
+        builder.Prompts.choice(session, msg, card.choices(consts.cards.sample_event), consts.styles.mr_button); 
     },
     (session, results) => {
 
         if(!results.response) {
             session.replaceDialog('/');
         } else {
-            session.replaceDialog('/Booking', 'test parameter');
+            var choice = results.response.entity;
+            var split = choice.split(':');
+
+            switch(split[0]) {
+                case 'IN':
+                    session.replaceDialog('/Events/Inclusions', { event_id: split[1] });
+                break;
+
+                case 'AV':
+                    session.replaceDialog('/Events/Availability', { event_id: split[1] });
+                break;
+                
+                default:
+                    session.replaceDialog('/Booking', { event_id: split[1] });
+            }
+            
         }
     }
 ]
@@ -74,12 +88,12 @@ module.exports.searchEvents = [
 
 module.exports.showInclusions = [
     (session, args) => {
-        console.log(args.event_id);
+        session.endConversation('ID ' + args.event_id);
     }
 ]
 
 module.exports.showAvailability = [
     (session, args) => {
-        console.log(args.event_id);
+        session.endConversation('ID' + args.event_id);
     }
 ]
