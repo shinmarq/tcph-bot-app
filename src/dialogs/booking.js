c7abe8922e4b127aa9a2c517d414771e22b16b79
shinmarq <shin@chatbot.ph>
2018-01-05 22:32:46 +0800

    test

--- src/dialogs/booking.js
+++ src/dialogs/booking.js
@@ -14,7 +14,7 @@ module.exports = [
         });
     },
     (session, results) => {
-        console.log(results);
+        console.log('test' + results);
         builder.Prompts.number(session, 'How many of you will join this event?');
     },
     (session, results) => {
