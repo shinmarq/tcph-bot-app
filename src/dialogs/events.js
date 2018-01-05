ae8768e2d2f5e157048bffb0b6023349e4c9d21b
shinmarq <shin@chatbot.ph>
2018-01-06 01:43:54 +0800

    test

--- src/dialogs/events.js
+++ src/dialogs/events.js
@@ -100,7 +100,7 @@ module.exports.showInclusions = [
 module.exports.showAvailability = [
     (session, args) => {
         api.availability(args.event_id, (res) => {
-            console.log(res)
+            console.log(args.event_id)
             session.endConversation(format('Here\'s the available dates and slots for this package: <br/><br/>{0}', card.availabilityDetails(res.data)));
         });
     }
