afbaa86bed6e2c0832330f6459738199272821aa
shinmarq <shin@chatbot.ph>
2018-01-06 12:41:19 +0800

    test

--- src/dialogs/booking.js
+++ src/dialogs/booking.js
@@ -7,6 +7,7 @@ const api = require('../helpers/apiRequest');
 
 module.exports = [
     (session, args, next) => {
+        // session.conversationData.body.client = args.event_id; // Get number of pax
         api.availability(args.event_id, (res) => {
             // var msg = card.eventAvailability(session, res.data);
             // console.log(card.idChoices(res.data));
@@ -19,15 +20,31 @@ module.exports = [
     },
     (session, results) => {
         var date = session.conversationData.dates[results.response.entity];
-        console.log(date); //date.event_id
+        session.conversationData.body.event_date = date.event_id // Get event id
+
         builder.Prompts.number(session, 'How many of you will join this event?');
     },
     (session, results) => {
+        session.conversationData.body.number_of_pax = results.response; // Get number of pax
+
         session.send('Alright got it!');
         builder.Prompts.number(session, 'Please enter your contact number');
     },
     (session, results) => {
-        session.send('Cool thanks!');
-        session.endConversation('Your booking is ready but you are not yet reserved for the slots \nplease settle downpayment Amounting: P5000 \n before 24hrs to these accounts');
+        session.conversationData.body.contact_number = results.response; // Get contact number
+        session.conversationData.body.fb_id = session.message.user.id; // Get fb id
+
+        api.userProfile(session.message.user.id, 'first_name last_name', (err, res) => {
+            session.conversationData.body.lead_guest = {
+                firstname: res.first_name,
+                lastname: res.last_name
+            } // Get lead guest dtl
+
+            console.log(session.conversationData.body);
+            session.send('Cool thanks!');
+            session.endConversation('Your booking is ready but you are not yet reserved for the slots \nplease settle downpayment Amounting: P5000 \n before 24hrs to these accounts');
+        });
+       
+        
     }
 ]
