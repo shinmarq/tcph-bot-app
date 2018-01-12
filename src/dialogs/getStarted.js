a99090a1c098cb7e377151ed8198f74ea36db172
shinmarq <shin@chatbot.ph>
2018-01-12 23:53:55 +0800

    test

--- src/dialogs/getStarted.js
+++ src/dialogs/getStarted.js
@@ -6,7 +6,7 @@ const card = require('../helpers/cardBuilder');
 const api = require('../helpers/apiRequest');
 
 module.exports = [
-    (session) => {
+    async(session) => {
         // api.userProfile(session.message.user.id, 'first_name', (err, res) => {
         //     // var cardName = card.getName(consts.cards.welcome);
         //     // var msg = card(session, consts.cards.welcome, cardName);
@@ -22,7 +22,7 @@ module.exports = [
         builder.Prompts.choice(session, format(consts.prompts.introduction, res.first_name), consts.choices.start, consts.styles.mr_button);
         
     },
-    (session, results) => {
+    async(session, results) => {
         if(!results.response) {
             session.replaceDialog('/');
         } else {
