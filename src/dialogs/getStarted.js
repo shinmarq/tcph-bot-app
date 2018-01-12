89b79da081917b9b0e8f055b73c9a99f5b9ec489
shinmarq <shin@chatbot.ph>
2018-01-13 00:53:55 +0800

    test

--- src/dialogs/getStarted.js
+++ src/dialogs/getStarted.js
@@ -15,15 +15,16 @@ module.exports = [
         //     session.send(format(consts.prompts.introduction, res.first_name));
         //     builder.Prompts.choice(session, format(consts.prompts.introduction, res.first_name), consts.choices.start, consts.styles.mr_button);
         // });
-        api.userProfile(session.message.user.id, 'first_name')
-        .then(res => {
-            console.log(res)
-            // session.send(format(consts.prompts.introduction, res.first_name));
-            // builder.Prompts.choice(session, format(consts.prompts.introduction, res.first_name), consts.choices.start, consts.styles.mr_button);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+        const res = api.userProfile(session.message.user.id, 'first_name');
+        console.log(res)
+        // .then(res => {
+        //     console.log(res)
+        //     // session.send(format(consts.prompts.introduction, res.first_name));
+        //     // builder.Prompts.choice(session, format(consts.prompts.introduction, res.first_name), consts.choices.start, consts.styles.mr_button);
+        // })
+        // .catch(err => {
+        //     console.log(err);
+        // });
         
     },
     (session, results) => {
