387523f75e62e1391f1a6f09ab731e28ae276d8e
shinmarq <shin@chatbot.ph>
2018-01-13 04:17:29 +0800

    test

--- src/dialogs/booking.js
+++ src/dialogs/booking.js
@@ -57,13 +57,14 @@ module.exports = [
         } else if(results.response.entity == choices[0]) {
             try {
                 const res = await event.createBooking(session.conversationData.body);
-                
+
                 session.endConversation(`You are now successfully booked for ${res.data.number_of_pax} for this package. this is your booking reference no. ${res.data.booking_refno} However, this slot is not yet reserved to you until you settle required down payment amounting ${res.data.number_of_pax * res.data.total_dp} 
                                         <br/><br/>BDO\ntest\n12345
                                         <br/><br/>Please send a photo of your receipt after the transfer before 24 hours or you booking will be expired. See you soon buddy 🙂
                                         `);
-            session.conversationData = {}
+                session.conversationData = {}
             } catch(err) {
+                console.log(err)
                 session.endConversation(err.message + ' :(');
                 session.conversationData = {}
             }
