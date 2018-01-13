2b324d43aaa80f3de0fdca327c7c18eceab18079
shinmarq <shin@chatbot.ph>
2018-01-13 09:40:24 +0800

    test

--- src/dialogs/default.js
+++ src/dialogs/default.js
@@ -5,6 +5,7 @@ const fb = require('../helpers/fb-helper')
 
 module.exports = [
     (session) => {
+        session.send(session.message.text);
         //Check if NLP present in sourceEvent
         // let entities = ('nlp' in session.message.sourceEvent.message) ? session.message.sourceEvent.message.nlp.entities : undefined;
         
