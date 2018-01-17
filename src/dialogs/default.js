a0184876417ce8135dc2c18d1bebeae533d88e55
shinmarq <shin@chatbot.ph>
2018-01-17 18:39:57 +0800

    test

--- src/dialogs/default.js
+++ src/dialogs/default.js
@@ -5,7 +5,26 @@ const fb = require('../helpers/fb-helper')
 
 module.exports = [
     (session) => {
-        actionDialog(session, session.message.text);
+        
+        var msg = session.message;
+        if (msg.attachments && msg.attachments.length > 0) {
+         // Echo back attachment
+         var attachment = msg.attachments[0];
+            session.send({
+                text: "You sent:",
+                attachments: [
+                    {
+                        contentType: attachment.contentType,
+                        contentUrl: attachment.contentUrl,
+                        name: attachment.name
+                    }
+                ]
+            });
+        } else {
+            // Echo back users text
+            // session.send("You said: %s", session.message.text);
+            actionDialog(session, session.message.text);
+        }
         //Check if NLP present in sourceEvent
         // let entities = ('nlp' in session.message.sourceEvent.message) ? session.message.sourceEvent.message.nlp.entities : undefined;
         
