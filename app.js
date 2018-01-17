a4315b9a7f9c90f6ad993b67318fd093eb5e1a47
shinmarq <shin@chatbot.ph>
2018-01-17 18:34:54 +0800

    test

--- app.js
+++ app.js
@@ -15,26 +15,7 @@ const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-const bot = new builder.UniversalBot(connector, (session) => {
-    var msg = session.message;
-    if (msg.attachments && msg.attachments.length > 0) {
-     // Echo back attachment
-     var attachment = msg.attachments[0];
-        session.send({
-            text: "You sent:",
-            attachments: [
-                {
-                    contentType: attachment.contentType,
-                    contentUrl: attachment.contentUrl,
-                    name: attachment.name
-                }
-            ]
-        });
-    } else {
-        // Echo back users text
-        session.send("You said: %s", session.message.text);
-    }
-});
+const bot = new builder.UniversalBot(connector);
 
 //=========================================================
 // Bots Middleware
